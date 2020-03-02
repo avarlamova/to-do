597a52a9623a29dfe74f3ba168ae611b9451e13a
avarlamova <59831804+avarlamova@users.noreply.github.com>
2020-03-02 14:13:10 +0300

    Add files via upload

---
+++ todo.js
@@ -0,0 +1,7 @@
+﻿let tasks = ["Сделать домашку", "Искупать кота", "Купить сельдерей"];
+function addTask() {
+    let newTask = prompt("Добавить новое важное дело", "введите задание");
+    tasks.push(newTask);
+    alert (tasks);
+}
+
