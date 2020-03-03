e56f1b29ca622055127d5907cb175d26070d06ab
avarlamova <59831804+avarlamova@users.noreply.github.com>
2020-03-03 23:29:24 +0300

    Update todo.js

--- todo.js
+++ todo.js
@@ -1,7 +1,15 @@
 ﻿let tasks = ["Сделать домашку", "Искупать кота", "Купить сельдерей"];
+
+const input = document.querySelector("input[type='text']");
+
 function addTask() {
-    let newTask = prompt("Добавить новое важное дело", "введите задание");
-    tasks.push(newTask);
-    alert (tasks);
+    const newTask = document.createElement("li");
+    document.body.append(newTask);
+   }
+function clearList () {
+  
 }
 
+    function getHelp() {
+
+    }
